2201958356bfc837ab75d3fa3be854333c7e562a
Mohamed Izhar <mohamed@izhar.xyz>
2024-11-16 17:41:50 +0530

    feat: add URL routes for retrieving all projects and testimonials

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import ContactViewSet
+from .views import ContactViewSet, ProjectViewSet, TestimonialViewSet
 
 urlpatterns = [
     path("contact/",
          ContactViewSet.as_view({"post": "send_contact_response"}), name="contact"),
+
+    path('projects/',
+         ProjectViewSet.as_view({'get': 'retrieve_all_projects'}), name='project-list'),
+
+    path('testimonials/', TestimonialViewSet.as_view(
+        {'get': 'retrieve_all_testimonials'}), name='testimonial-list'),
 ]

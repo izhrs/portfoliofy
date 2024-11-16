b765be9b23755eb66585c9831c479c3c59b5e344
Mohamed Izhar <mohamed@izhar.xyz>
2024-11-16 17:40:41 +0530

    feat: add Project and Testimonial models with Cloudinary integration for image management

--- core/models.py
+++ core/models.py
@@ -1,3 +1,44 @@
+import cloudinary
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='projects/')
+    source_code_url = models.URLField(blank=True)
+    site_url = models.URLField(blank=True)
+
+    detail = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def delete(self, *args, **kwargs):
+        # Delete the image from Cloudinary
+        if self.image:
+            cloudinary.uploader.destroy(self.image.name)
+        super().delete(*args, **kwargs)
+
+
+class Testimonial(models.Model):
+    name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
+    quote = models.TextField()
+    avatar = models.ImageField(upload_to='testimonials/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def delete(self, *args, **kwargs):
+        # Delete the image from Cloudinary
+        if self.avatar:
+            cloudinary.uploader.destroy(self.avatar.name)
+        super().delete(*args, **kwargs)

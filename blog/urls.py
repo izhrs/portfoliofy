945d8ef86e56db43ebae45f8c23de7f57d12c592
Mohamed Izhar <mohamed@izhar.xyz>
2024-11-17 18:11:21 +0530

    fix: update URL patterns for blog posts and categories to use plural forms

--- blog/urls.py
+++ blog/urls.py
@@ -6,14 +6,14 @@ urlpatterns = [
     # Blog Post
     path("posts/",
          BlogViewSet.as_view({"get": "retrieve_all_posts"}), name="all-posts"),
-    path("post/featured/",
+    path("posts/featured/",
          BlogViewSet.as_view({"get": "retrieve_featured_post"}), name="featured-posts"),
-    path("post/<slug:slug>/",
+    path("posts/<slug:slug>/",
          BlogViewSet.as_view({"get": "retrieve_post_detail"}), name="post-detail"),
 
     # Blog Category
     path("categories/",
          BlogViewSet.as_view({"get": "retrieve_all_categories"}), name="all-categories"),
-    path("category/<slug:slug>/",
+    path("categories/<slug:slug>/",
          BlogViewSet.as_view({"get": "retrieve_posts_by_category"}), name="posts-by-category"),
 ]

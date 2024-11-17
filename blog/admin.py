2d58daa80a53df1abeb7fef9c442ac23f40cbe98
Mohamed Izhar <mohamed@izhar.xyz>
2024-11-17 15:36:39 +0530

    feat: add admin interface for blog and post models with custom inlines and display options

---
+++ blog/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.db import models
+from unfold.admin import ModelAdmin, StackedInline
+from unfold.contrib.forms.widgets import WysiwygWidget
+
+from .models import Category, Post, PostContent
+
+
+class PostContentInline(StackedInline):
+    model = PostContent
+    extra = 1
+
+    formfield_overrides = {
+        models.TextField: {
+            "widget": WysiwygWidget,
+        }
+    }
+
+
+class PostAdmin(ModelAdmin):
+    list_display = ('title', 'get_categories', 'created_at', 'updated_at')
+    list_filter = ('categories', 'created_at')
+    search_fields = ('title', 'categories__name', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [PostContentInline]
+
+    # Custom method to display categories in list view
+    def get_categories(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+    get_categories.short_description = 'Categories'
+
+
+class CategoryAdmin(ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
